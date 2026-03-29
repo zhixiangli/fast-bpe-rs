ddf4d60f74af5f3409aabe95660fd93e64f35399
Zhixiang Li <zhixiang202@gmail.com>
2026-03-29 20:22:46 +0800

    feat: add default fancy-regex split pattern (#84)
    * feat: add default fancy-regex split pattern
    
    * fix: handle fancy-regex iterator errors explicitly
    
    * fix: implement Default for BPE

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use std::fmt;
 #[derive(Debug)]
 pub enum BPEError {
     /// The caller supplied an invalid regex for splitting documents into chunks.
-    InvalidSplitRegex(regex::Error),
+    InvalidSplitRegex(Box<fancy_regex::Error>),
     /// A special token attempted to reuse an id reserved for raw byte values.
     SpecialTokenIdOverlapsBaseVocab { token: String, token_id: TokenId },
     /// Two different special tokens were assigned the same token id.
@@ -29,8 +29,8 @@ impl fmt::Display for BPEError {
 
 impl std::error::Error for BPEError {}
 
-impl From<regex::Error> for BPEError {
-    fn from(value: regex::Error) -> Self {
-        Self::InvalidSplitRegex(value)
+impl From<fancy_regex::Error> for BPEError {
+    fn from(value: fancy_regex::Error) -> Self {
+        Self::InvalidSplitRegex(Box::new(value))
     }
 }
